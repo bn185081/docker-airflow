4b29f13631e0e94543bbd6f66137aaa95ceb757f
bn185081 <97611615+bn185081@users.noreply.github.com>
2024-01-02 16:05:39 +0200

    testing self trigger dag

--- dags/self_trigger_dag.py
+++ dags/self_trigger_dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator, ShortCircuitOperator
-from airflow.operators.bash import BashOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 from datetime import datetime
@@ -8,11 +7,13 @@ from datetime import datetime
 default_args = {"start_date": datetime(2021, 1, 1)}
 
 
-def _start():
+def _start(**kwargs):
     print("Start Step")
+    print(f"context params: {kwargs['params']}")
+    print(f"conf: {kwargs['dag_run'].conf}")
 
 
-def _get_update_date_period():
+def _get_update_date_period(**kwargs):
     print("Getting the period of time when the BQ changed")
 
 
@@ -20,10 +21,13 @@ def _regular_wf():
     print("Regular WF")
 
 
-def _check_for_data_updates(**context):
+def _check_for_data_updates(**kwargs):
     print("Check for data Updates")
-    print(f'AF context: {context}')
-    return not context["dag_run"].external_trigger
+    print(f'AF context: {kwargs}')
+    kwargs['ti'].xcom_push(key="data_update_start_date", value="2023-12-01")
+    kwargs['ti'].xcom_push(key="data_update_end_date", value="2023-12-31")
+    # return not kwargs["dag_run"].external_trigger
+    return True
 
 with DAG(
     "self_trigger_dag", schedule_interval="@daily", default_args=default_args, catchup=False
@@ -36,28 +40,15 @@ with DAG(
         task_id="GetUpdateDates", python_callable=_get_update_date_period
     )
 
-    interval = BashOperator(task_id="Interval"
-                            , bash_command="sleep 60"
-                            , retries=1)
-
     check_for_data_updates = ShortCircuitOperator(task_id='Check_Data_Updates', python_callable=_check_for_data_updates)
 
     trigger = TriggerDagRunOperator(
         task_id='Trigger_Self',
         trigger_dag_id=dag.dag_id,
-        # trigger_run_id='dataUpdateTrigger__{{ ts }}') # Check if I can give a special name in this case
-        conf={'data_update_start_date': "2023-12-01", 'data_update_end_date': "2023-12-31"},
+        # trigger_run_id='dataUpdateTrigger__' + {{ ts }},  # TODO Brenda Check if I can give a special name in this case
+        conf={"payload": "{{ task_instance.xcom_pull(task_ids='_check_for_data_updates') }}"},
     )
 
     start >> regular_wf >> get_dates >> check_for_data_updates >> trigger
 
-    # trigger_target = TriggerDagRunOperator(
-    #     task_id="trigger_target",
-    #     trigger_dag_id=target_dag_id,
-    #     execution_date="2023-12-25T10:09:10.896248+00:00",
-    #     reset_dag_run=True,
-    #     wait_for_completion=True,  # To make sure the target dag finishes
-    #     # deferrable=True, # Need to set something in the environment for this to work
-    #     conf={},
-    # )
 
